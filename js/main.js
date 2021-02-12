4141cd314186de3fec54fae56affae4eeb372d1e
Rade <you@example.com>
2021-02-12 11:12:39 +0100

    Menu Hamm Test

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+    //javascript for navigation bar effect on scroll
+    window.addEventListener("scroll", function(){
+      var nav = document.querySelector("nav");
+      nav.classList.toggle('sticky', window.scrollY > 0);
+    });
+
+    //javascript for responsive navigation sidebar menu
+    var menu = document.querySelector('.menu');
+    var menuBtn = document.querySelector('.menu-btn');
+    var closeBtn = document.querySelector('.close-btn');
+
+    menuBtn.addEventListener("click", () => {
+      menu.classList.add('active');
+    });
+
+    closeBtn.addEventListener("click", () => {
+      menu.classList.remove('active');
+    });
+
+    
+
+
+
+// Showing Elements
 window.addEventListener('scroll', reveal);
 
 function reveal(){
